cad8760a4625dbf102a8313e5a5aaaaccf3dfcd0
Alex <alex.ackerman@me.com>
2015-03-31 21:17:55 -0700

    Initial Commit

---
+++ WindowExperiment/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WindowExperiment
+//
+//  Created by Alex on 3/31/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ WindowExperiment/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  WindowExperiment
+//
+//  Created by Alex on 3/31/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
